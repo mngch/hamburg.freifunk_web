0937704b8fab5dc6f66066a8e6d59d03ff76f36a
Sebastian Beyer <sebastian.beyer@zmaw.de>
2013-01-26 00:21:51 +0100

    smaller font sizes on mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
-(function(){var e;e=["http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"],require(e,function(){return $(function(){return console.log("jquery loaded, dom ready")})})}).call(this);
+(function() {
+  var js;
+
+  js = ["http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"];
+
+  require(js, function() {
+    return $(function() {
+      return console.log('jquery loaded, dom ready');
+    });
+  });
+
+}).call(this);
